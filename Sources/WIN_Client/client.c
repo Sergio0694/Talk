4ee4285e72e311967d0bdd9337ad0af3c687caac
AnSlvt <andrea.slvt94@gmail.com>
2016-08-11 18:07:31 +0200

    Fixed the thread exiting

--- Sources/WIN_Client/client.c
+++ Sources/WIN_Client/client.c
@@ -23,8 +23,10 @@
 
 typedef struct thread_params_s
 {
-    int* user_index;
+    guid_t* output_guid;
+    string_t* username;
     HANDLE output_sem;
+    HANDLE race_condition_sem;
 } thread_params_t;
 
 /* ======== GLOBALS ======== */
@@ -34,32 +36,6 @@ HANDLE consoleWindow = NULL;
 SOCKET socketd = INVALID_SOCKET;
 int chat_window_port;
 /* ========================= */
-
-/*
-// Thread function to handle the pressing of the return key
-DWORD return_handler(LPVOID params)
-{
-    int ret;
-
-    thread_params_t* args = (thread_params_t*)params;
-    HANDLE stdin_handle = args->stdin_handle;
-    HANDLE event = args->event_handle;
-
-    while (TRUE)
-    {
-        ret = WaitForSingleObject(stdin_handle, INFINITE);
-        ERROR_HELPER(ret == WAIT_FAILED, "Error in WaitForSingleObject");
-        if (GetAsyncKeyState(VK_RETURN))
-        {
-            printf("DEBUG \\n key pressed - signaling ... \n");
-            ret = SetEvent(event);
-            ERROR_HELPER(ret == 0, "Error while signaling the event");
-            ret = ResetEvent(event);
-            ERROR_HELPER(ret == 0, "Error while resetting the event");
-        }
-    }
-}
-*/
 
 HANDLE prepare_chat_window()
 {
@@ -191,74 +167,173 @@ int read_integer()
     return number >= 0 ? number : -1;
 }
 
+// First picker function that checks for a request by another user
 DWORD WINAPI picker_handler_in(LPVOID arg)
 {
-    // receive the username of the client who have picked you
+    // Unpack the arguments
+    thread_params_t* parameters = (thread_params_t*)arg;
+    HANDLE semaphore = parameters->output_sem;
+    HANDLE race_semaphore = parameters->race_condition_sem;
+    string_t* username = (string_t*)parameters->username;
+
+    // Receive the username of the client who picked you
     printf("DEBUG: data on socket - trying to read\n");
     char buffer[BUFFER_LENGTH];
     int read = recv_from_socket(socketd, buffer, BUFFER_LENGTH);
+
+    // Save the return value
+    DWORD ret = WaitForSingleObject(race_semaphore, INFINITE);
+    ERROR_HELPER(ret == WAIT_FAILED, "Error waiting for the semaphore");
+    parameters->output_guid = NULL;
+
+    // Copy the username actual value
     *username = (char*)malloc(sizeof(char) * read);
     strncpy(*username, buffer, read);
     printf("DEBUG: %s\n", *username);
-    CloseHandle(input_handles[0]);
-    return NULL;
+
+    // Unlock the caller
+    ReleaseSemaphore(
+        semaphore, // Semaphore to release
+        1, // Release increment
+        NULL // I don't care about the previous semaphore count
+    );
+    return TRUE;
+}
+
+// Second picker function that checks for a manual user request
+DWORD WINAPI picker_handler_out(LPVOID arg)
+{
+    // Unpack the arguments
+    thread_params_t* parameters = (thread_params_t*)arg;
+    HANDLE semaphore = parameters->output_sem;
+    HANDLE race_semaphore = parameters->race_condition_sem;
+
+    // Get the target user index
+    guid_t* return_value;
+    while (TRUE)
+    {
+        // Read an integer from stdin require conversion and i need inlining for use TerminateThread
+        char buf[10];
+        const int maxIntCharLen = 10;
+        char* res = fgets(buf, maxIntCharLen, stdin);
+        ERROR_HELPER(res == NULL, "Error reading from the input buffer");
+        if (strncmp(buf, "R", 1) == 0)
+        {
+            printf("DEBUG refresh requested\n");
+            return REFRESH;
+        }
+        int i = 0;
+        while (buf[i] != '\n')
+        {
+            if (!isdigit((int)buf[i++])) return -1;
+        }
+        int number = atoi(buf);
+        printf("%d\n", number);
+
+        // The target value has been converted, go for the checks
+        int target = number;
+        if (target == -1)
+        {
+            printf("The selected index isn't valid\n");
+            continue;
+        }
+        else if (target == REFRESH)
+        {
+            printf("DEBUG returning (guid_t*)REFRESH\n");
+            return_value = (guid_t*)REFRESH;
+            break;
+        }
+        printf("DEBUG trying get guid\n");
+        guid_t* guid = try_get_guid(client_users_list, target);
+        if (guid == NULL) printf("The input index isn't valid\n");
+        else
+        {
+            return_value = guid;
+            break;
+        }
+    }
+
+    // Save the return values
+    DWORD ret = WaitForSingleObject(race_semaphore, INFINITE);
+    ERROR_HELPER(ret == WAIT_FAILED, "Error waiting for the semaphore");
+    parameters->output_guid = return_value;
+    parameters->username = NULL;
+
+    // Unlock the caller
+    ReleaseSemaphore(
+        semaphore, // Semaphore to release
+        1, // Release increment
+        NULL // I don't care about the previous semaphore count
+    );
+    return TRUE;
 }
 
 guid_t* pick_target_user(string_t* username)
 {
     printf("Pick a user to connect to: ");
 
+    // Prepare the semaphore to synchronize the caller and the two threads
+    HANDLE picker_semaphore = CreateSemaphore(
+        NULL, // Default security attributes
+        0, // Initial count to 0 to lock the caller function
+        1, // Maximum count if both threads return at the same time
+        NULL // Unnamed semaphore
+    );
+    ERROR_HELPER(picker_semaphore == NULL, "Error creating the semaphore");
+
+    // Prepare the semaphore to synchronize the caller and the two threads
+    HANDLE race_semaphore = CreateSemaphore(
+        NULL, // Default security attributes
+        1, // Initial count to 1
+        1, // Same value for the maximum count
+        NULL // Unnamed semaphore
+    );
+    ERROR_HELPER(picker_semaphore == NULL, "Error creating the race semaphore");
+
+    // Prepare the struct to pass the parameters to the two threads
     thread_params_t arg = { 0 };
-    arg.user_index = (int*)malloc(sizeof(int));
-    *(arg.user_index) = -1;
+    arg.username = username;
+    arg.output_guid = NULL;
+    arg.output_sem = picker_semaphore;
+    arg.race_condition_sem = race_semaphore;
 
+    // Create the two picker threads
     HANDLE picker_threads[2];
+    picker_threads[0] = CreateThread(
+        NULL,       // Default security attributes
+        0,          // Default stack size
+        (LPTHREAD_START_ROUTINE) picker_handler_in, // Handler function
+        (LPVOID)&arg, // Handler arguments
+        0,          // Default creation flags
+        NULL); // Ignore the thread identifier
+    ERROR_HELPER(picker_threads[0] == NULL, "Error creating the first picker thread");
 
-    while (TRUE)
-    {
-        // Wait for both the socket and the stdin
-        int ret = WaitForMultipleObjects(2, input_handles, FALSE, INFINITE);
-        ERROR_HELPER(ret == WAIT_FAILED || ret == WAIT_TIMEOUT,
-            "Error in the WaitForMultipleObjects call");
-        int index = ret;
+    picker_threads[1] = CreateThread(
+        NULL,       // Default security attributes
+        0,          // Default stack size
+        (LPTHREAD_START_ROUTINE) picker_handler_out, // Handler function
+        (LPVOID)&arg, // Handler arguments
+        0,          // Default creation flags
+        NULL); // Ignore the thread identifier
+    ERROR_HELPER(picker_threads[1] == NULL, "Error creating the second picker thread");
 
-        // Check if another user has started a chat session with this client
-        if (index == 0)
-        {
-            // receive the username of the client who have picked you
-            printf("DEBUG: data on socket - trying to read\n");
-            char buffer[BUFFER_LENGTH];
-            int read = recv_from_socket(socketd, buffer, BUFFER_LENGTH);
-            *username = (char*)malloc(sizeof(char) * read);
-            strncpy(*username, buffer, read);
-            printf("DEBUG: %s\n", *username);
-            CloseHandle(input_handles[0]);
-            return NULL;
-        }
-        else if (index == 1)
-        {
-            int target = read_integer();
-            if (target == -1)
-            {
-                printf("The selected index isn't valid\n");
-                continue;
-            }
-            else if (target == REFRESH)
-            {
-                printf("DEBUG returning (guid_t*)REFRESH\n");
-                CloseHandle(input_handles[1]);
-                return (guid_t*)REFRESH;
-            }
-            printf("DEBUG trying get guid\n");
-            guid_t* guid = try_get_guid(client_users_list, target);
-            if (guid == NULL) printf("The input index isn't valid\n");
-            else
-            {
-                CloseHandle(input_handles[0]);
-                return guid;
-            }
-        }
-    }
+    // Wait until at least one of the two spawned threads has returned
+    DWORD ret = WaitForSingleObject(picker_semaphore, INFINITE);
+    ERROR_HELPER(ret == WAIT_FAILED, "Error waiting for the semaphore");
+
+    // Get the return value
+    guid_t* return_value = arg.output_guid;
+    printf("DEBUG the return value is NULL? %d\n", return_value == NULL);
+
+    // Cleanup
+    ERROR_HELPER(!(TerminateThread(picker_threads[0], TRUE)
+        || TerminateThread(picker_threads[1], TRUE)),
+        "Error closing the picker threads");
+    ERROR_HELPER((CloseHandle(picker_semaphore) && CloseHandle(race_semaphore)) == FALSE,
+        "Error closing the semaphores");
+
+    // Finally return the result
+    return return_value;
 }
 
 void send_target_guid(const guid_t guid)
@@ -344,10 +419,10 @@ void chat(string_t username)
             if (gets_ret == NULL) continue;
             send_to_socket(socketd, message);
 
-            // Send the message to the chat window
-            char chat_window_message[BUFFER_LENGTH];
+            // Send the message to the chat window CHANGED: Seems to double the message displayed
+            /*char chat_window_message[BUFFER_LENGTH];
             snprintf(chat_window_message, BUFFER_LENGTH, "0[Me]%s", message);
-            send_to_socket(chat_window_socket, chat_window_message);
+            send_to_socket(chat_window_socket, chat_window_message);*/
         }
     }
 
@@ -377,20 +452,6 @@ BOOL CtrlHandler(DWORD fdwCtrlType)
         close_res = CloseHandle(consoleWindow);
         ERROR_HELPER(!close_res, "Error closing the chat window");
     }
-
-    /*
-    // Close the thread, if necessary
-    if (return_thread != NULL)
-    {
-        close_res = CloseHandle(return_thread);
-        ERROR_HELPER(!close_res, "Error closing the return thread");
-    }
-    if (event_handle != NULL)
-    {
-        close_res = CloseHandle(event_handle);
-        ERROR_HELPER(!close_res, "Error closing the event handle");
-    }
-    */
 
     // Confirm and return the result
     printf("SHUT DOWN completed\n");
